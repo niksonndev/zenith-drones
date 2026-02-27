d2ebd19d1a2c53e11224f19200b51afd76257f61
niksonndev <niksonndev@gmail.com>
2026-02-27 17:37:43 -0300

    feat(products): add react-hot-toast and dispached the reducer

--- components/Products.tsx
+++ components/Products.tsx
@@ -1,15 +1,26 @@
 import { ShoppingCartIcon } from '@heroicons/react/outline';
 import Image from 'next/image';
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import { urlFor } from '../sanity';
+import { addToBasket } from '../redux/basketSlice';
+import toast from 'react-hot-toast';
 
 interface Props {
   product: Product;
 }
 
 function Products({ product }: Props) {
-  const addItemToBasket = () => {};
-  
+  const dispatch = useDispatch();
+
+  const addItemToBasket = () => {
+    dispatch(addToBasket(product));
+
+    toast.success(`${product.title} added to basket`, {
+      position: 'bottom-center',
+    });
+  };
+
   return (
     <div className='flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10'>
       <div className='relative h-64 w-full md:h-72'>
