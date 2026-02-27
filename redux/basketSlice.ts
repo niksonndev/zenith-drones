430b4834e6b03bacfc80c45ec00e51545e2e47a6
niksonndev <niksonndev@gmail.com>
2026-02-27 17:37:43 -0300

    feat(store): added basket slice with remove from basket reducer

--- redux/basketSlice.ts
+++ redux/basketSlice.ts
@@ -1,5 +1,6 @@
 import { createSlice } from '@reduxjs/toolkit';
 import type { PayloadAction } from '@reduxjs/toolkit';
+import { RootState } from './store';
 
 export interface BasketState {
   items: Product[];
@@ -16,9 +17,41 @@ export const basketSlice = createSlice({
     addToBasket: (state: BasketState, action: PayloadAction<Product>) => {
       state.items = [...state.items, action.payload];
     },
+    removeFromBasket: (
+      state: BasketState,
+      action: PayloadAction<{ id: string }>
+    ) => {
+      const index = state.items.findIndex(
+        (item: Product) => item._id === action.payload.id
+      );
+
+      let newBasket = [...state.items];
+
+      if (index >= 0) {
+        newBasket.splice(index, 1);
+      } else {
+        console.log(
+          `Cant remove product (id: ${action.payload.id}) as its not in the basket!`
+        );
+      }
+
+      state.items = newBasket;
+    },
   },
 });
 
-export const { addToBasket } = basketSlice.actions;
+export const { addToBasket, removeFromBasket } = basketSlice.actions;
+
+// Selectors -> retrieving items in state to use in different components
+export const selectBasketItems = (state: RootState) => state.basket.items;
+export const selectBasketItemsWithId = (state: RootState, id: string) => {
+  state.basket.items.filter((item: Product) => item._id === id);
+};
+export const selectBasketTotal = (state: RootState) => {
+  state.basket.items.reduce(
+    (total: number, item: Product) => (total += item.price),
+    0
+  );
+};
 
 export default basketSlice.reducer;
