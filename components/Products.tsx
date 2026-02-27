f5bd600131d087006c5e282b19f977f263ef0ae1
niksonndev <niksonndev@gmail.com>
2026-02-27 17:37:43 -0300

    feat(product): added the product component

---
+++ components/Products.tsx
@@ -0,0 +1,23 @@
+import Image from 'next/image';
+import React from 'react';
+import { urlFor } from '../sanity';
+
+interface Props {
+  product: Product;
+}
+
+function Products({ product }: Props) {
+  return (
+    <div className='flex h-fit w-[320px] select-none flex-col space-y-3 rounded-xl bg-[#35383C] p-8 md:h-[500px] md:w-[400px] md:p-10'>
+      <div className='relative h-64 w-full md:h-72'>
+        <Image
+          src={urlFor(product.image[0]).url()}
+          layout='fill'
+          objectFit='contain'
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Products;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,25 @@
-import type { GetServerSideProps, NextPage } from 'next';
+import { Tab } from '@headlessui/react';
+import type { GetServerSideProps } from 'next';
 import Head from 'next/head';
 import Image from 'next/image';
 import Header from '../components/Header';
 import Landing from '../components/Landing';
-import { Tab } from '@headlessui/react';
+import Products from '../components/Products';
+import { fetchCategories } from '../utils/fetchCategories';
+import { fetchProducts } from '../utils/fetchProducts';
 
-const Home: NextPage = () => {
+interface Props {
+  categories: Category[];
+  products: Product[];
+}
+
+const Home = ({ categories, products }: Props) => {
+  const showProducts = (category: number) => {
+    return products
+      .filter((product) => product.category._ref === categories[category]._id)
+      .map((product) => <Products product={product} key={product._id} />); // filter products by category
+  };
+
   return (
     <div className=''>
       <Head>
@@ -26,10 +40,27 @@ const Home: NextPage = () => {
 
           <Tab.Group>
             <Tab.List className='flex justify-center'>
-              <Tab>iPhone</Tab>
+              {categories.map((category) => (
+                <Tab
+                  key={category._id}
+                  id={category._id}
+                  className={({ selected }) =>
+                    `whitespace-nowrap rounded-t-lg py-3 px-5 text-sm font-light outline-none md:py-4 md:px-6 md:text-base ${
+                      selected
+                        ? 'borderGradient bg-[#35383C] text-white'
+                        : 'border[#35383C] border-b-2 text-[#747474]'
+                    }`
+                  }
+                >
+                  {category.title}
+                </Tab>
+              ))}
             </Tab.List>
             <Tab.Panels className='mx-auto max-w-fit pt-10 pb-24 sm:px-4'>
-              <Tab.Panel className='tabPanel'></Tab.Panel>
+              <Tab.Panel className='tabPanel'>{showProducts(0)}</Tab.Panel>
+              <Tab.Panel className='tabPanel'>{showProducts(1)}</Tab.Panel>
+              <Tab.Panel className='tabPanel'>{showProducts(2)}</Tab.Panel>
+              <Tab.Panel className='tabPanel'>{showProducts(3)}</Tab.Panel>
             </Tab.Panels>
           </Tab.Group>
         </div>
@@ -41,10 +72,14 @@ const Home: NextPage = () => {
 export default Home;
 
 // Backend code
-export const getServerSideProps: GetServerSideProps = async () => {
-  /* const categories = await fetchCategories() */
+export const getServerSideProps: GetServerSideProps<Props> = async () => {
+  const categories = await fetchCategories();
+  const products = await fetchProducts();
 
   return {
-    props: {},
+    props: {
+      categories,
+      products,
+    },
   };
 };
